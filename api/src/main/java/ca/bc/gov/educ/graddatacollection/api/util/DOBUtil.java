1561be193d286c44fc4bc45c68571c48a466b99c
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2024-09-18 14:58:35 -0700

    initial

---
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/util/DOBUtil.java
@@ -0,0 +1,28 @@
+package ca.bc.gov.educ.graddatacollection.api.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
+public class DOBUtil {
+
+    private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("uuuuMMdd").withResolverStyle(ResolverStyle.STRICT);
+
+    private DOBUtil() {
+    }
+
+    public static boolean isValidDate(String dob) {
+        if (StringUtils.isEmpty(dob)) {
+            return false;
+        }
+        try {
+            LocalDate.parse(dob, format);
+        } catch (DateTimeParseException ex) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/util/ValidationUtil.java
@@ -0,0 +1,41 @@
+package ca.bc.gov.educ.graddatacollection.api.util;
+
+import ca.bc.gov.educ.graddatacollection.api.exception.InvalidPayloadException;
+import ca.bc.gov.educ.graddatacollection.api.exception.errors.ApiError;
+import lombok.val;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.validation.FieldError;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+public class ValidationUtil {
+
+  private static final String INVALID_CHARS = "'-.";
+
+  private ValidationUtil(){
+
+  }
+
+  public static FieldError createFieldError(String objectName, String fieldName, Object rejectedValue, String message) {
+    return new FieldError(objectName, fieldName, rejectedValue, false, null, null, message);
+  }
+
+  public static void validatePayload(Supplier<List<FieldError>> validator) {
+    val validationResult = validator.get();
+    if (!validationResult.isEmpty()) {
+      ApiError error = ApiError.builder().timestamp(LocalDateTime.now()).message("Payload contains invalid data.").status(BAD_REQUEST).build();
+      error.addValidationErrors(validationResult);
+      throw new InvalidPayloadException(error);
+    }
+  }
+
+  public static boolean containsInvalidChars(String name) {
+    Pattern pattern = Pattern.compile("^[a-z\\-.'\\s]+$", Pattern.CASE_INSENSITIVE);
+    return StringUtils.isNotEmpty(name) && (!pattern.matcher(name).matches() || name.replaceAll("[" + INVALID_CHARS + "]","").isEmpty());
+  }
+}
