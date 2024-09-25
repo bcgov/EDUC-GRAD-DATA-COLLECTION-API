ac09a4f873de29d3f3a89054a7def0285051c4f7
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2024-09-25 11:40:04 -0700

    start of course restrictions entity

--- api/src/main/java/ca/bc/gov/educ/graddatacollection/api/model/v1/CourseRestrictionsEntity.java
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/model/v1/CourseRestrictionsEntity.java
@@ -14,22 +14,22 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 /**
- * The type Course.
+ * The type Course Restrictions.
  */
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name = "COURSE")
+@Table(name = "COURSE_RESTRICTIONS")
 @DynamicUpdate
-public class CourseEntity {
+public class CourseRestrictionsEntity {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator", parameters = {
             @org.hibernate.annotations.Parameter(name = "uuid_gen_strategy_class", value = "org.hibernate.id.uuid.CustomVersionOneStrategy")})
-    @Column(name = "COURSE_ID", unique = true, updatable = false, columnDefinition = "BINARY(16)")
-    UUID courseId;
+    @Column(name = "COURSE_RESTRICTIONS_ID", unique = true, updatable = false, columnDefinition = "BINARY(16)")
+    UUID courseRestrictionsId;
 
     @NotNull(message = "code cannot be null")
     @Column(name = "COURSE_CODE")
@@ -39,33 +39,23 @@ public class CourseEntity {
     @Column(name = "COURSE_LEVEL")
     String courseLevel;
 
-    @NotNull(message = "name cannot be null")
-    @Column(name = "COURSE_NAME")
-    String courseName;
+    @NotNull(message = "restricted code cannot be null")
+    @Column(name = "RESTRICTED_COURSE_CODE")
+    String restrictedCourseCode;
 
-    @NotNull(message = "num credits cannot be null")
-    @Column(name = "NUM_CREDITS")
-    String numCredits;
-
-    @NotNull(message = "language cannot be null")
-    @Column(name = "COURSE_LANGUAGE")
-    String courseLanguage;
+    @NotNull(message = "restricted level cannot be null")
+    @Column(name = "RESTRICTED_COURSE_LEVEL")
+    String restrictedCourseLevel;
 
     @PastOrPresent
-    @NotNull(message = "course start date cannot be null")
-    @Column(name = "COURSE_START_DATE")
-    LocalDateTime courseStartDate;
+    @NotNull(message = "restriction start date cannot be null")
+    @Column(name = "RESTRICTION_START_DATE")
+    LocalDateTime restrictionStartDate;
 
     @PastOrPresent
-    @NotNull(message = "course end date cannot be null")
-    @Column(name = "COURSE_END_DATE")
-    LocalDateTime courseEndDate;
-
-    @Column(name = "WORK_EXPERIENCE")
-    Boolean workExperience;
-
-    @Column(name = "COURSE_TYPE_GENERIC")
-    String courseTypeGeneric;
+    @NotNull(message = "restriction end date cannot be null")
+    @Column(name = "RESTRICTION_END_DATE")
+    LocalDateTime restrictionEndDate;
 
     @PastOrPresent
     @Column(name = "CREATE_DATE", updatable = false)
