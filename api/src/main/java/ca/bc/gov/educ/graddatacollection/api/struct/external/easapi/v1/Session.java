0c7280553da6bff777ce456b44c5345f2d79fb3f
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2024-10-21 16:06:48 -0700

    Get assessment sessions

---
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/struct/external/easapi/v1/Session.java
@@ -0,0 +1,41 @@
+package ca.bc.gov.educ.graddatacollection.api.struct.external.easapi.v1;
+
+import ca.bc.gov.educ.graddatacollection.api.struct.v1.BaseRequest;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.ReadOnlyProperty;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Session extends BaseRequest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ReadOnlyProperty
+    private String sessionID;
+
+    @ReadOnlyProperty
+    private String schoolYear;
+
+    @ReadOnlyProperty
+    private Integer courseMonth;
+
+    @ReadOnlyProperty
+    private Integer courseYear;
+
+    @NotNull(message = "activeFromDate cannot be null")
+    private LocalDateTime activeFromDate;
+
+    @NotNull(message = "activeUntilDate cannot be null")
+    private LocalDateTime activeUntilDate;
+}
