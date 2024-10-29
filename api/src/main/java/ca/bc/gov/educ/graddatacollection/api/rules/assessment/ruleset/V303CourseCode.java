01cf3cac16d4cefb2f94c5c21035336f2c782b8e
Marco Villeneuve <arcshiftsolutions@gmail.com>
2024-10-28 17:36:23 -0700

    Added V303

---
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/rules/assessment/ruleset/V303CourseCode.java
@@ -0,0 +1,62 @@
+package ca.bc.gov.educ.graddatacollection.api.rules.assessment.ruleset;
+
+import ca.bc.gov.educ.graddatacollection.api.rules.StudentValidationIssueSeverityCode;
+import ca.bc.gov.educ.graddatacollection.api.rules.assessment.AssessmentStudentValidationFieldCode;
+import ca.bc.gov.educ.graddatacollection.api.rules.assessment.AssessmentStudentValidationIssueTypeCode;
+import ca.bc.gov.educ.graddatacollection.api.rules.assessment.AssessmentValidationBaseRule;
+import ca.bc.gov.educ.graddatacollection.api.service.v1.AssessmentRulesService;
+import ca.bc.gov.educ.graddatacollection.api.struct.v1.AssessmentStudentValidationIssue;
+import ca.bc.gov.educ.graddatacollection.api.struct.v1.StudentRuleData;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  | ID   | Severity | Rule                                                                  | Dependent On |
+ *  |------|----------|-----------------------------------------------------------------------|--------------|
+ *  | V303 | WARNING  | The Assessment Code provided is not valid for the Assessment Session  |--------------|
+ *                      specified.
+ */
+@Component
+@Slf4j
+@Order(120)
+public class V303CourseCode implements AssessmentValidationBaseRule {
+
+    private final AssessmentRulesService assessmentRulesService;
+
+    public V303CourseCode(AssessmentRulesService assessmentRulesService) {
+        this.assessmentRulesService = assessmentRulesService;
+    }
+
+    @Override
+    public boolean shouldExecute(StudentRuleData studentRuleData, List<AssessmentStudentValidationIssue> validationErrorsMap) {
+        log.debug("In shouldExecute of V303: for assessment {} and assessmentStudentID :: {}", studentRuleData.getAssessmentStudentEntity().getAssessmentID() ,
+                studentRuleData.getAssessmentStudentEntity().getAssessmentStudentID());
+
+        var shouldExecute = true;
+
+        log.debug("In shouldExecute of V303: Condition returned - {} for assessmentStudentID :: {}" ,
+                shouldExecute,
+                studentRuleData.getAssessmentStudentEntity().getAssessmentStudentID());
+
+        return  shouldExecute;
+    }
+
+    @Override
+    public List<AssessmentStudentValidationIssue> executeValidation(StudentRuleData studentRuleData) {
+        var student = studentRuleData.getAssessmentStudentEntity();
+        log.debug("In executeValidation of V303 for assessmentStudentID :: {}", student.getAssessmentStudentID());
+        final List<AssessmentStudentValidationIssue> errors = new ArrayList<>();
+
+        if (!assessmentRulesService.courseIsValidForSession(student.getCourseYear(), student.getCourseMonth(), student.getCourseCode())) {
+            log.debug("V303: The Assessment Code provided is not valid for the Assessment Session specified for assessmentStudentID :: {}", student.getAssessmentStudentID());
+            errors.add(createValidationIssue(StudentValidationIssueSeverityCode.ERROR, AssessmentStudentValidationFieldCode.COURSE_CODE, AssessmentStudentValidationIssueTypeCode.COURSE_CODE_INVALID));
+        }
+        return errors;
+    }
+
+}
--- api/src/main/java/ca/bc/gov/educ/graddatacollection/api/service/v1/AssessmentRulesService.java
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/service/v1/AssessmentRulesService.java
@@ -1,8 +1,10 @@
 package ca.bc.gov.educ.graddatacollection.api.service.v1;
 
 import ca.bc.gov.educ.graddatacollection.api.repository.v1.DemographicStudentRepository;
+import ca.bc.gov.educ.graddatacollection.api.rest.RestUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -12,6 +14,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class AssessmentRulesService {
 
+    private final RestUtils restUtils;
     private final DemographicStudentRepository demographicStudentRepository;
 
     public boolean containsDemographicDataForStudent(UUID incomingFilesetID, String pen, String surname, String localID) {
@@ -19,4 +22,15 @@ public class AssessmentRulesService {
         return !results.isEmpty();
     }
 
+    public boolean courseIsValidForSession(String year, String month, String courseCode){
+        if(StringUtils.isEmpty(year) || StringUtils.isEmpty(month) || StringUtils.isEmpty(courseCode)){
+            return false;
+        }
+        var session = restUtils.getAssessmentSessionByCourseMonthAndYear(month, year);
+
+        if(session.isPresent()){
+            return session.get().getAssessments().stream().anyMatch(assessment -> assessment.getAssessmentTypeCode().equalsIgnoreCase(courseCode));
+        }
+        return false;
+    }
 }
