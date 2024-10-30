99cbcde83a98b726ea31cec21920a71dee7615c6
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2024-10-30 14:14:13 -0700

    constants

--- api/src/main/java/ca/bc/gov/educ/graddatacollection/api/constants/SagaEnum.java
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/constants/SagaEnum.java
@@ -2,6 +2,7 @@ package ca.bc.gov.educ.graddatacollection.api.constants;
 
 public enum SagaEnum {
     PROCESS_DEM_STUDENTS_SAGA,
-    PROCESS_COURSE_STUDENTS_SAGA
+    PROCESS_COURSE_STUDENTS_SAGA,
+    PROCESS_ASSESSMENT_STUDENTS_SAGA
 
 }
