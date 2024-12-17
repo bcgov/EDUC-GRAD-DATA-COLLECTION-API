79646bf1d5c426beda31e3677a864ce77dee82c3
keeganbcgov <Keegan.Laur@gov.bc.ca>
2024-12-17 10:35:00 -0800

    EGX-38: purging stale Incoming Filesets.

---
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/schedulers/PurgeStaleIncomingFilesetsScheduler.java
@@ -0,0 +1,36 @@
+package ca.bc.gov.educ.graddatacollection.api.schedulers;
+
+import ca.bc.gov.educ.graddatacollection.api.service.v1.IncomingFilesetService;
+import jakarta.transaction.Transactional;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.LockAssert;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Component
+@Slf4j
+public class PurgeStaleIncomingFilesetsScheduler {
+    @Getter(PRIVATE)
+    private final IncomingFilesetService incomingFilesetService;
+
+    public PurgeStaleIncomingFilesetsScheduler(final IncomingFilesetService incomingFilesetService) {
+        this.incomingFilesetService = incomingFilesetService;
+    }
+
+    /**
+     * Run the job based on configured scheduler(a cron expression) and purge old records from DB.
+     */
+    @Scheduled(cron = "${scheduled.jobs.purge.stale.incoming.filesets.cron}")
+    @SchedulerLock(name = "PurgeStaleIncomingFilesetsLock", lockAtLeastFor = "${scheduled.jobs.purge.stale.incoming.filesets.cron.lockAtLeastFor}", lockAtMostFor = "${scheduled.jobs.purge.stale.incoming.filesets.cron.lockAtMostFor}")
+    @Transactional
+    public void pollIncomingFilesetTableAndPurgeExpiredRecords() {
+        LockAssert.assertLocked();
+        log.info("Purging stale Incoming Filesets records from EDUC-STUDENT-DATA-COLLECTION-SAGA-API.");
+        this.incomingFilesetService.purgeStaleIncomingFilesetRecords();
+        log.info("Finished purging stale Incoming Filesets records from EDUC-STUDENT-DATA-COLLECTION-SAGA-API");
+    }
+}
