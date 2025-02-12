78e2ff0ecccb7638405da357de266b14e4fa31bc
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2025-02-12 12:09:10 -0800

    Update CustomSearchType.java

--- api/src/main/java/ca/bc/gov/educ/graddatacollection/api/constants/v1/CustomSearchType.java
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/constants/v1/CustomSearchType.java
@@ -2,11 +2,6 @@ package ca.bc.gov.educ.graddatacollection.api.constants.v1;
 
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
 
 @Getter
 public enum CustomSearchType {
