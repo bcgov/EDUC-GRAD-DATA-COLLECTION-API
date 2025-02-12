0c01578bd7084222a6162e82190f316d4206705a
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2025-02-12 14:33:02 -0800

    update

--- api/src/main/java/ca/bc/gov/educ/graddatacollection/api/rules/assessment/ruleset/V315CourseType.java
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/rules/assessment/ruleset/V315CourseType.java
@@ -47,7 +47,7 @@ public class V315CourseType implements AssessmentValidationBaseRule {
 
         if (StringUtils.isNotBlank(student.getCourseType())){
             log.debug("V315: Course type value is ignored and must be blank :: {}", student.getAssessmentStudentID());
-            errors.add(createValidationIssue(StudentValidationIssueSeverityCode.WARNING, ValidationFieldCode.COURSE_TYPE, AssessmentStudentValidationIssueTypeCode.COURSE_TYPE_NOT_BLANK, AssessmentStudentValidationIssueTypeCode.COURSE_TYPE_NOT_BLANK.getMessage()));
+            errors.add(createValidationIssue(StudentValidationIssueSeverityCode.WARNING, ValidationFieldCode.CRSE_TYPE, AssessmentStudentValidationIssueTypeCode.COURSE_TYPE_NOT_BLANK, AssessmentStudentValidationIssueTypeCode.COURSE_TYPE_NOT_BLANK.getMessage()));
         }
         return errors;
     }
