d55f41d603ea321370a00d53dcec789624be89a0
keeganbcgov <Keegan.Laur@gov.bc.ca>
2025-02-24 13:57:58 -0800

    V304 messaging rewording.

--- api/src/main/java/ca/bc/gov/educ/graddatacollection/api/rules/assessment/AssessmentStudentValidationIssueTypeCode.java
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/rules/assessment/AssessmentStudentValidationIssueTypeCode.java
@@ -10,8 +10,8 @@ public enum AssessmentStudentValidationIssueTypeCode {
   DEM_DATA_MISSING("DEM_DATA_MISSING", "This student appears in the XAM file but is missing from the DEM file. The student's assessment registrations will not be updated."),
   COURSE_LEVEL_NOT_BLANK("COURSE_LEVEL_NOT_BLANK", "Course level value is ignored and must be blank."),
   COURSE_CODE_INVALID("COURSE_CODE_INVALID", "The Assessment Code provided is not valid for the Assessment Session specified."),
-  COURSE_SESSION_DUP("COURSE_SESSION_DUP", "The assessment session is a duplicate of an existing assessment session for this student/assessment/level."),
-  COURSE_SESSION_EXCEED("COURSE_SESSION_EXCEED", "Student has already reached the maximum number of writes for this Assessment."),
+  COURSE_SESSION_DUP("COURSE_SESSION_DUP", "The student has already received a Proficiency Score / Special Case for this assessment session."),
+  COURSE_SESSION_EXCEED("COURSE_SESSION_EXCEED", "The student has reached the maximum number of writes for %s. The registration will not be updated."),
   INTERIM_SCHOOL_PERCENTAGE_NOT_BLANK("INTERIM_SCHOOL_PERCENTAGE_NOT_BLANK", "Interim school percentage value is ignored and must be blank."),
   INTERIM_LETTER_GRADE_NOT_BLANK("INTERIM_LETTER_GRADE_NOT_BLANK", "Interim letter grade value is ignored and must be blank."),
   FINAL_SCHOOL_PERCENTAGE_NOT_BLANK("FINAL_SCHOOL_PERCENTAGE_NOT_BLANK", "Final school percentage value is ignored and must be blank."),
@@ -25,7 +25,7 @@ public enum AssessmentStudentValidationIssueTypeCode {
   EXAM_SCHOOL_INVALID("EXAM_SCHOOL_INVALID", "Invalid assessment center provided."),
   DUPLICATE_XAM_RECORD("DUPLICATE_XAM_RECORD", "There are more than one CODE/SESSION DATE registrations in the file for the same student."),
   COURSE_CODE_CSF("COURSE_CODE_CSF", "Student is enrolled in a Programme Francophone school and the registration is for a French Immersion assessment. This registration will not be updated."),
-  COURSE_CODE_NON_CSF("COURSE_CODE_NON_CSF", "Student is not enrolled in a Programme Francophone school and the registration is for LTP12."),
+  COURSE_CODE_NON_CSF("COURSE_CODE_NON_CSF", "The student has written, attempted to write or has a special case code for %s. The assessment session will not be withdrawn."),
   COURSE_ALREADY_WRITTEN("COURSE_ALREADY_WRITTEN", "Assessment has been written by the student, withdrawal is not allowed."),
   DEM_ISSUE("DEM_ISSUE", "Student XAM record will not be processed due to an issue with the student's demographics.");
 
