ca1d2604749ca27b2ef6620a62b6709dd9ed7795
Erin Eckerman <erineckerman@gmail.com>
2025-04-09 15:50:39 -0700

    change to isGraduated

--- api/src/main/java/ca/bc/gov/educ/graddatacollection/api/rules/demographic/ruleset/BlankGradRequirementRule.java
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/rules/demographic/ruleset/BlankGradRequirementRule.java
@@ -55,9 +55,9 @@ public class BlankGradRequirementRule implements DemographicValidationBaseRule {
         boolean isNotGradeGA = student.getGrade() != null && !student.getGrade().equalsIgnoreCase(SchoolGradeCodes.GRADUATED_ADULT.getCode());
         boolean isNotIndependentFNS = schoolCategory != null && !schoolCategory.equalsIgnoreCase(SchoolCategoryCodes.INDP_FNS.getCode());
         boolean gradRequirementYearIsBlank = StringUtils.isBlank(student.getGradRequirementYear());
-        boolean isNotGraduated = gradRecord == null || StringUtils.isNotBlank(gradRecord.getProgramCompletionDate());
+        boolean isGraduated = gradRecord != null && StringUtils.isNotBlank(gradRecord.getProgramCompletionDate());
 
-        if (gradRequirementYearIsBlank && (isNotGradeGA || isNotIndependentFNS) && isNotGraduated) {
+        if (gradRequirementYearIsBlank && (isNotGradeGA || isNotIndependentFNS) && isGraduated) {
             log.debug("StudentProgram-D12: Null program not valid for demographicStudentID :: {}", student.getDemographicStudentID());
             errors.add(createValidationIssue(StudentValidationIssueSeverityCode.ERROR, ValidationFieldCode.GRAD_REQUIREMENT_YEAR, DemographicStudentValidationIssueTypeCode.STUDENT_PROGRAM_GRAD_REQUIREMENT_YEAR_NULL, DemographicStudentValidationIssueTypeCode.STUDENT_PROGRAM_GRAD_REQUIREMENT_YEAR_NULL.getMessage()));
         }
