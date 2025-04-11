ff8691fb2bb59301884bdb974091b70faae1c362
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2025-04-11 14:12:37 -0700

    sonar fix

--- api/src/main/java/ca/bc/gov/educ/graddatacollection/api/batch/service/ProcessSummerStudentService.java
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/batch/service/ProcessSummerStudentService.java
@@ -1,5 +1,7 @@
 package ca.bc.gov.educ.graddatacollection.api.batch.service;
 
+import ca.bc.gov.educ.graddatacollection.api.batch.exception.FileError;
+import ca.bc.gov.educ.graddatacollection.api.batch.exception.FileUnProcessableException;
 import ca.bc.gov.educ.graddatacollection.api.batch.mappers.BatchFileMapper;
 import ca.bc.gov.educ.graddatacollection.api.batch.struct.GradStudentCourseDetails;
 import ca.bc.gov.educ.graddatacollection.api.batch.struct.GradStudentCourseFile;
@@ -7,14 +9,18 @@ import ca.bc.gov.educ.graddatacollection.api.batch.struct.GradStudentDemogDetail
 import ca.bc.gov.educ.graddatacollection.api.batch.struct.GradStudentDemogFile;
 import ca.bc.gov.educ.graddatacollection.api.batch.validation.GradFileValidator;
 import ca.bc.gov.educ.graddatacollection.api.constants.v1.FilesetStatus;
+import ca.bc.gov.educ.graddatacollection.api.constants.v1.GradCollectionStatus;
 import ca.bc.gov.educ.graddatacollection.api.constants.v1.SchoolCategoryCodes;
 import ca.bc.gov.educ.graddatacollection.api.exception.EntityNotFoundException;
+import ca.bc.gov.educ.graddatacollection.api.exception.InvalidPayloadException;
+import ca.bc.gov.educ.graddatacollection.api.exception.errors.ApiError;
 import ca.bc.gov.educ.graddatacollection.api.model.v1.IncomingFilesetEntity;
 import ca.bc.gov.educ.graddatacollection.api.model.v1.ReportingPeriodEntity;
 import ca.bc.gov.educ.graddatacollection.api.repository.v1.ReportingPeriodRepository;
 import ca.bc.gov.educ.graddatacollection.api.service.v1.IncomingFilesetService;
 import ca.bc.gov.educ.graddatacollection.api.struct.v1.SummerFileUpload;
 import ca.bc.gov.educ.graddatacollection.api.struct.v1.SummerStudentData;
+import ca.bc.gov.educ.graddatacollection.api.util.ValidationUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
@@ -22,10 +28,14 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.FieldError;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Service
 @RequiredArgsConstructor
@@ -34,26 +44,51 @@ public class ProcessSummerStudentService {
     private final IncomingFilesetService incomingFilesetService;
     private final ReportingPeriodRepository reportingPeriodRepository;
     private final GradFileValidator gradFileValidator;
+    public static final String INVALID_PAYLOAD_MSG = "Payload contains invalid data.";
+    public static final String GRAD_FILE_UPLOAD = "gradFileUpload";
     private static final BatchFileMapper mapper = BatchFileMapper.mapper;
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void process(final SummerFileUpload summerUpload, final String schoolID, final String districtID) {
-        String incomingSchoolID = schoolID;
-        String incomingDistrictID = districtID;
-        if(districtID != null) {
-            var mincode = summerUpload.getSummerStudents().getFirst();
-            var schoolTombstone = gradFileValidator.getSchoolUsingMincode(mincode.getSchoolCode());
-            incomingSchoolID = schoolTombstone.get().getSchoolId();
-        } else {
-            var schoolTombstone =  gradFileValidator.getSchool(schoolID);
-            if(!SchoolCategoryCodes.INDEPENDENTS_AND_OFFSHORE.contains(schoolTombstone.get().getSchoolCategoryCode())) {
-                incomingDistrictID = schoolTombstone.get().getDistrictId();
+        final var guid = UUID.randomUUID().toString();
+        try {
+            String incomingSchoolID = schoolID;
+            String incomingDistrictID = districtID;
+            if (districtID != null) {
+                var mincode = summerUpload.getSummerStudents().getFirst();
+                var schoolTombstone = gradFileValidator.getSchoolUsingMincode(mincode.getSchoolCode());
+                if(schoolTombstone.isPresent()) {
+                    incomingSchoolID = schoolTombstone.get().getSchoolId();
+                } else {
+                    throw new FileUnProcessableException(FileError.INVALID_SCHOOL, guid, GradCollectionStatus.LOAD_FAIL, String.valueOf(mincode));
+                }
+            } else {
+                var schoolTombstone = gradFileValidator.getSchoolByID(guid, schoolID);
+                if (!SchoolCategoryCodes.INDEPENDENTS_AND_OFFSHORE.contains(schoolTombstone.getSchoolCategoryCode())) {
+                    incomingDistrictID = schoolTombstone.getDistrictId();
+                }
             }
-        }
 
-        GradStudentDemogFile demFile = createDemFile(summerUpload.getSummerStudents());
-        GradStudentCourseFile courseFile = createCourseFile(summerUpload.getSummerStudents());
-        createAndSaveIncomingFileset(summerUpload, incomingSchoolID, incomingDistrictID, demFile, courseFile);
+            GradStudentDemogFile demFile = createDemFile(summerUpload.getSummerStudents());
+            GradStudentCourseFile courseFile = createCourseFile(summerUpload.getSummerStudents());
+            createAndSaveIncomingFileset(summerUpload, incomingSchoolID, incomingDistrictID, demFile, courseFile);
+        } catch(final FileUnProcessableException fileUnProcessableException) {
+            log.error("File could not be processed exception :: {}", fileUnProcessableException);
+            ApiError error = ApiError.builder().timestamp(LocalDateTime.now()).message(INVALID_PAYLOAD_MSG).status(BAD_REQUEST).build();
+            var validationError = ValidationUtil.createFieldError(GRAD_FILE_UPLOAD, schoolID, fileUnProcessableException.getReason());
+            List<FieldError> fieldErrorList = new ArrayList<>();
+            fieldErrorList.add(validationError);
+            error.addValidationErrors(fieldErrorList);
+            throw new InvalidPayloadException(error);
+        } catch (final Exception e) {
+            log.error("Exception while processing the file with guid :: {} :: Exception :: {}", guid, e);
+            ApiError error = ApiError.builder().timestamp(LocalDateTime.now()).message(INVALID_PAYLOAD_MSG).status(BAD_REQUEST).build();
+            var validationError = ValidationUtil.createFieldError(GRAD_FILE_UPLOAD, schoolID, FileError.GENERIC_ERROR_MESSAGE.getMessage());
+            List<FieldError> fieldErrorList = new ArrayList<>();
+            fieldErrorList.add(validationError);
+            error.addValidationErrors(fieldErrorList);
+            throw new InvalidPayloadException(error);
+        }
     }
 
     public GradStudentDemogFile createDemFile(final List<SummerStudentData> summerStudents) {
