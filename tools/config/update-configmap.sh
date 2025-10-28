f4865f778898321bac957eeb74375e951112a146
arcshiftsolutions <arcshiftsolutions@gmail.com>
2025-10-28 09:48:43 -0700

    Fix to script

--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -13,13 +13,10 @@ CHES_TOKEN_URL="${11}"
 CHES_ENDPOINT_URL="${12}"
 GRAD_NAMESPACE="${13}"
 EDX_URL="${14}"
-COREG_NAMPESPACE="${15}"
+COREG_NAMESPACE="${15}"
 TZVALUE="America/Vancouver"
 SOAM_KC_REALM_ID="master"
 SOAM_KC=soam-$envValue.apps.silver.devops.gov.bc.ca
-
-echo COREG_NAMESPACE value is $COREG_NAMESPACE
-echo COREG_NAMPESPACE value is $COREG_NAMPESPACE
 
 SOAM_KC_LOAD_USER_ADMIN=$(oc -n $COMMON_NAMESPACE-$envValue -o json get secret sso-admin-${envValue} | sed -n 's/.*"username": "\(.*\)"/\1/p' | base64 --decode)
 SOAM_KC_LOAD_USER_PASS=$(oc -n $COMMON_NAMESPACE-$envValue -o json get secret sso-admin-${envValue} | sed -n 's/.*"password": "\(.*\)",/\1/p' | base64 --decode)
