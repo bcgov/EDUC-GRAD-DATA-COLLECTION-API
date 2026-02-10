f18c1b54d87c0819c054fc8a3088fdb6c40b7d16
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2026-02-10 11:16:18 -0800

    FinalIncomingFilesetPurgeRepository

---
+++ api/src/main/java/ca/bc/gov/educ/graddatacollection/api/repository/v1/FinalIncomingFilesetPurgeRepository.java
@@ -0,0 +1,22 @@
+package ca.bc.gov.educ.graddatacollection.api.repository.v1;
+
+import ca.bc.gov.educ.graddatacollection.api.model.v1.FinalIncomingFilesetPurgeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Repository
+public interface FinalIncomingFilesetPurgeRepository extends JpaRepository<FinalIncomingFilesetPurgeEntity, UUID>, JpaSpecificationExecutor<FinalIncomingFilesetPurgeEntity> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM FinalIncomingFilesetPurgeEntity WHERE createDate <= :deleteDate")
+    void deleteWithCreateDateBefore(LocalDateTime deleteDate);
+}
+
